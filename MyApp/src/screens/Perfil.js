d9c4d5233e9b7757b7e4d6ef3f81198eeb221700
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-15 10:12:43 -0300

    FALTA use params en todos y perfil que sea ocion editar tmb

--- MyApp/src/screens/Perfil.js
+++ MyApp/src/screens/Perfil.js
@@ -6,31 +6,66 @@ import { useNavigation } from '@react-navigation/native';
 const Perfil = ({idUsuario}) => {
     const navigation = useNavigation();
     const [Usuario, setUsuario] = useState({});
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
 
-    axios
+    const buttonOnsubmitHandler = () => {
+        axios
             .get("http://localhost:3000/updateUsuarioInfo", {
-                id:idUsuario,
+                id: idUsuario,
+                Nombre: nombre,
+                Apellido: apellido
             })
             .then(
                 (response) => {
                     if (response.status === 200) {
                         console.log(response);
                         setUsuario(response.data.products);
+                        navigation.navigate("Home", idUsuario)
                     }
                 },
                 (res) => {
                     setError(res.response.data);
                 }
             );
+    }
 
     return (
         <View>
-            <Text>Perfil</Text>
+            <Text style = {styles.title}>PERFIL</Text>
+            <TextInput
+                value={nombre}
+                style ={styles.textInput}
+                onChangeText={setNombre}
+                placeholder="Nombre"
+                secureTextEntry={false}
+            />
+
+            <TextInput
+                style ={styles.textInput}
+                value={apellido}
+                onChangeText={setApellido}
+                placeholder="Apellido"
+                secureTextEntry={false}
+            />
+
+            <TouchableOpacity onPress={buttonOnsubmitHandler} style={styles.button}> 
+            <Text style = {{marginLeft:'27%', fontSize:16, fontWeight:'500'}}>Guardar</Text>
+            </TouchableOpacity>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    title:{
+        backgroundColor:'#E742EB', width: '50%', color:'white',  fontWeight:'500', fontSize:16, textAlign:'center', marginTop:10, marginLeft:'25%', padding: 15,borderRadius:15,
+    },
+    button:{
+        backgroundColor:'#fda3ff', width: '30%', color:'black',  marginTop:10, marginLeft:'35%', padding: 12,borderRadius:15,
+    },
+    textInput:{
+        color:'white', marginTop:20, width:'80%', marginLeft:'10%'
+    }
 });
 
 export default Perfil;   
