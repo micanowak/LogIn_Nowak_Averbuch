77adcae4cecc68d5db1f2e2be099ac67362af9d3
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-22 08:56:32 -0300

    intentando recibir parametros en home

--- MyApp/src/screens/Home.js
+++ MyApp/src/screens/Home.js
@@ -2,37 +2,41 @@ import React, { useState } from "react";
 import { TextInput, View, Text, Button, StyleSheet, TouchableOpacity } from "react-native-web";
 import axios from "axios";
 import { useNavigation } from '@react-navigation/native';
+import { render } from "react-dom";
 
-const Home = ({idUsuario}) => {
+const Home = () => {
     const navigation = useNavigation();
     const [Usuario, setUsuario] = useState({});
+    const [idUsuario, setIdUsuario] = useState({});
+    setIdUsuario(this.props.route.params.id);
+    console.log(idUsuario);
 
     const [perfilCompleto, setPerfilCompleto] = useState(false);
 
-    const perfilOnClickHandler = () =>{
+    const perfilOnClickHandler = () => {
         navigation.navigate("Perfil", idUsuario);
     }
 
     axios
-            .get("http://localhost:3000/getUsuarioById", {
-                id:idUsuario,
-            })
-            .then(
-                (response) => {
-                    if (response.status === 200) {
-                        console.log(response);
-                        setUsuario(response.data.products);
+        .get("http://localhost:3000/getUsuarioById", {
+            id: idUsuario,
+        })
+        .then(
+            (response) => {
+                if (response.status === 200) {
+                    console.log(response);
+                    setUsuario(response.data.products);
 
-                        if(Usuario.Nombre != undefined && Usuario.Apellido != undefined){
-                            setPerfilCompleto(true);
-                        }
-
+                    if (Usuario.Nombre != undefined && Usuario.Apellido != undefined) {
+                        setPerfilCompleto(true);
                     }
-                },
-                (res) => {
-                    setError(res.response.data);
+
                 }
-            );
+            },
+            (res) => {
+                setError(res.response.data);
+            }
+        );
 
     return (
         <View>
