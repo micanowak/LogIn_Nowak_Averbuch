58a27c9e77c5be4e9679fa90cd9f19f7f9b85905
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-03 12:01:46 -0300

    le falta bastante

--- MyApp/src/screens/LogIn.js
+++ MyApp/src/screens/LogIn.js
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
 import { TextInput, View, Text, Button, StyleSheet, TouchableOpacity } from "react-native-web";
-import axios from "axios";
 import { useNavigation } from '@react-navigation/native';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDYzKjC_JD3imqZjacsJvEsp8nvb6KkPLo",
+    authDomain: "login-nowak-averbuch-smaevich.firebaseapp.com",
+    projectId: "login-nowak-averbuch-smaevich",
+    storageBucket: "login-nowak-averbuch-smaevich.appspot.com",
+    messagingSenderId: "290640125257",
+    appId: "1:290640125257:web:dbad063d65630f10c3ce98",
+    measurementId: "G-5NNCX5BSP0"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const LogIn = () => {
     const navigation = useNavigation();
@@ -11,30 +25,30 @@ const LogIn = () => {
     const [Mensaje, setMensaje] = useState('');
     const [error, setError] = useState("");
 
-    const buttonOnsubmitHandler = () => {
+    const buttonOnsubmitHandler = async () => {
         setButtonPressed(true);
-        axios
-            .post("http://localhost:3000/logInUsuario", {
-                username: nombreUsuario,
-                password: contrasenia,
-            })
-            .then(
-                (response) => {
-                    if (response.status === 200) {
-                        //esUsuario = true;
-                        console.log(response);
-                        setMensaje("usuario correcto");
-                        console.log(response.data.id);
-                        navigation.navigate("Home", response.data.id, response.data.nombre, response.data.apellido);
-                    }
-                },
-                (res) => {
-                    setError(res.response.data);
-                    setMensaje("usuario incorrecto");
-                }
-            );
-    };
 
+        try {
+            const userCredential = await firebase.auth().signInWithEmailAndPassword(nombreUsuario, contrasenia);
+
+            if (userCredential) {
+                const userId = userCredential.user.uid;
+
+                const db = firebase.firestore();
+                const userDoc = await db.collection('usuarios').doc(userId).get();
+
+                if (userDoc.exists) {
+                    const userData = userDoc.data();
+                    navigation.navigate("Home", userId, userData.nombre, userData.apellido);
+                } else {
+                    setMensaje("Usuario no encontrado");
+                }
+            }
+        } catch (error) {
+            setError(error.message);
+            setMensaje("Inicio de sesión incorrecto");
+        }
+    };
 
     const onPressHandler = () => {
         navigation.navigate("SignIn");
@@ -42,29 +56,29 @@ const LogIn = () => {
 
     return (
         <View>
-            <Text style = {styles.title}>INICIO DE SESIÓN</Text>
+            <Text style={styles.title}>INICIO DE SESIÓN</Text>
             <TextInput
                 value={nombreUsuario}
-                style ={styles.textInput}
+                style={styles.textInput}
                 onChangeText={setNombreUsuario}
                 placeholder="Nombre de Usuario"
                 secureTextEntry={false}
             />
 
             <TextInput
-                style ={styles.textInput}
+                style={styles.textInput}
                 value={contrasenia}
                 onChangeText={setContrasenia}
                 placeholder="Contraseña"
                 secureTextEntry={true}
             />
 
-            <TouchableOpacity onPress={buttonOnsubmitHandler} style={styles.button}> 
-            <Text style = {{marginLeft:'27%', fontSize:16, fontWeight:'500'}}>Entrar</Text>
+            <TouchableOpacity onPress={buttonOnsubmitHandler} style={styles.button}>
+                <Text style={{ marginLeft: '27%', fontSize: 16, fontWeight: '500' }}>Entrar</Text>
             </TouchableOpacity>
-                
+
             <TouchableOpacity onPress={onPressHandler} style={styles.buttonSecond}>
-                <Text  style = {{marginLeft:'18%'}}>Registrarse</Text>
+                <Text style={{ marginLeft: '18%' }}>Registrarse</Text>
             </TouchableOpacity>
             {ButtonPressed ? <p>{Mensaje}</p> : <p></p>}
         </View>
@@ -72,18 +86,18 @@ const LogIn = () => {
 };
 
 const styles = StyleSheet.create({
-    title:{
-        backgroundColor:'#E742EB', width: '50%', color:'white',  fontWeight:'500', fontSize:16, textAlign:'center', marginTop:10, marginLeft:'25%', padding: 15,borderRadius:15,
+    title: {
+        backgroundColor: '#E742EB', width: '50%', color: 'white', fontWeight: '500', fontSize: 16, textAlign: 'center', marginTop: 10, marginLeft: '25%', padding: 15, borderRadius: 15,
     },
-    textInput:{
-        color:'white', marginTop:20, width:'80%', marginLeft:'10%'
+    textInput: {
+        color: 'white', marginTop: 20, width: '80%', marginLeft: '10%'
     },
-    button:{
-        backgroundColor:'#fda3ff', width: '30%', color:'black',  marginTop:10, marginLeft:'35%', padding: 12,borderRadius:15,
+    button: {
+        backgroundColor: '#fda3ff', width: '30%', color: 'black', marginTop: 10, marginLeft: '35%', padding: 12, borderRadius: 15,
     },
-    buttonSecond:{
-        backgroundColor:'#f6c9f7', width: '30%', color:'black',  marginTop:10, marginLeft:'35%', padding: 5,borderRadius:15,
+    buttonSecond: {
+        backgroundColor: '#f6c9f7', width: '30%', color: 'black', marginTop: 10, marginLeft: '35%', padding: 5, borderRadius: 15,
     }
 });
 
-export default LogIn;   
+export default LogIn;
